6c60bf6b3b3a542ff81789284b08150f68fc74d9
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-07-18 11:10:46 +0530

    merge sort program added

--- Program.js
+++ Program.js
@@ -221,7 +221,6 @@ console.log('\n------JavaScript call() method--------')
 // without param
 //let fullName = person.fullName.call(gPerson)
 //console.log(fullName)
-
 const sPerson = {
     fName: 'Sarbjeet',
     lName: 'kaur'
@@ -230,5 +229,40 @@ const sPerson = {
 let fullName = person.fullNameWithCity.call(sPerson,"Chandigarh")
 console.log(fullName)
 
+function mergeSort(arr1, arr2) {
+    let result = [];
+    let i=0;
+    let j=0;
+    while(i< arr1.length && j < arr2.length) {
+      if(arr1[i] < arr2[j]){
+        result.push(arr1[i])
+        i++;
+      } else {
+        result.push(arr2[j])
+        j++;
+      }
+    }
+   while(i < arr1.length) {
+      result.push(arr1[i])
+      i++;
+   }
+   while(j < arr2.length){
+      result.push(arr2[j])
+      j++;
+   }    
+    return result
+}
+
+function merge(arr){
+    if(arr.length <= 1) 
+        return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = merge( arr.slice(0, mid));
+    let right = merge(arr.slice(mid))
+    return mergeSort(left,right)
+}
+
+let mergeSArr = [4, 8, 7, 2, 11, 1, 3];
+console.log('Merge Sort output -> ', merge(mergeSArr))
 
   
