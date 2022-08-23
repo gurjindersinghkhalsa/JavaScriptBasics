a02c7d043a80e066804fba3060b103d0740ef8a8
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-08-23 22:20:45 +0530

    program to check Anagram string addd

--- Program.js
+++ Program.js
@@ -295,3 +295,59 @@ function sortWithSelection() {
 
 sortWithSelection()
   
+
+let NO_OF_CHARS = 256;
+ 
+/* function to check whether two strings
+    are anagram of each other */
+function areAnagram(str1, str2)
+{
+ 
+    // Create 2 count arrays and initialize
+        // all values as 0
+        let count1 = new Array(NO_OF_CHARS);
+        let count2 = new Array(NO_OF_CHARS);
+        for(let i = 0; i < NO_OF_CHARS; i++)
+        {
+            count1[i] = 0;
+            count2[i] = 0;
+        }
+         
+        let i;
+  
+        // For each character in input strings,
+        // increment count in the corresponding
+        // count array
+        for (i = 0; i < str1.length && i < str2.length;
+             i++) {
+            count1[str1[i].charCodeAt(0)]++;
+            count2[str1[i].charCodeAt(0)]++;
+        }
+  
+        // If both strings are of different length.
+        // Removing this condition will make the program
+        // fail for strings like "aaca" and "aca"
+        if (str1.length != str2.length)
+            return false;
+  
+        // Compare count arrays
+        for (i = 0; i < NO_OF_CHARS; i++)
+            if (count1[i] != count2[i])
+                return false;
+  
+        return true;
+}
+let str1 = ("abcd").split("");
+let str2 = ("bcda").split("");
+  
+        // Function call
+if (areAnagram(str1, str2)) {
+  console.log('Yes');
+}
+else {
+  console.log('No');
+}
+
+
+
+
